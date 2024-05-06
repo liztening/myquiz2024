02560f955611bf436e88449c1aede7e4871d93cc
Lunatix14 <102019966+Lunatix14@users.noreply.github.com>
2024-05-06 19:35:52 +0700

    แก้ไข Wording ตอนเฉลยคำตอบ

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ function checkAnswer1(answer1) {
     if (answer1 === 'a') {
     result.textContent = "ถูกต้อง! PIM ย่อมาจาก PIM";
     result.style.color = 'darkgreen';
-    alert (answer1);
     } else {
-    result.textContent = "Incorrect! Try again.";
+    result.textContent = "ผิด! ลองตอบอีกที";
     result.style.color = 'darkred';
     }
     return answer1;
@@ -17,8 +16,18 @@ function checkAnswer2(answer2) {
     result.textContent = "ถูกต้อง! Gosoft คือมูลนิธิที่ไม่แสวงหาผลกำไร!";
     result.style.color = 'darkgreen';
     } else {
-    result.textContent = "Incorrect! Try again.";
+    result.textContent = "ผิด! โลกสวยเกินไปแล้ว";
     result.style.color = 'darkred';
     }
     return answer2;
     }
+
+let point = answer1 + answer2;
+
+function totalScore(point) {
+    if (point === 'ad') {
+    alert('เกย์วิปลาส');
+    } else {
+    alert('โบ้งหัวแร้ง');   
+    }
+}
