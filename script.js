fc640bee2eb6141d1784e090d8e8b53fc0f26a30
Lunatix14 <102019966+Lunatix14@users.noreply.github.com>
2024-05-06 16:51:17 +0700

    เพิ่ม Quiz 2

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-function checkAnswer(answer) {
-    const result = document.getElementById('result');
-    if (answer === 'a') {
-    result.textContent = "ถูกตอง PIM ยอมาจาก PIM";
-    result.style.color = 'green';
+function checkAnswer1(answer1) {
+    const result = document.getElementById('result1');
+    if (answer1 === 'a') {
+    result.textContent = "ถูกต้อง! PIM ย่อมาจาก PIM";
+    result.style.color = 'darkgreen';
+    alert (answer1);
     } else {
     result.textContent = "Incorrect! Try again.";
-    result.style.color = 'red';
+    result.style.color = 'darkred';
     }
+    return answer1;
+    }
+
+function checkAnswer2(answer2) {
+    const result = document.getElementById('result2');
+    if (answer2 === 'd') {
+    result.textContent = "ถูกต้อง! Gosoft คือมูลนิธิที่ไม่แสวงหาผลกำไร!";
+    result.style.color = 'darkgreen';
+    } else {
+    result.textContent = "Incorrect! Try again.";
+    result.style.color = 'darkred';
+    }
+    return answer2;
     }
