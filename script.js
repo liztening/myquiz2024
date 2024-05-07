adf1ef22438e71e732a940e5597ad7816ac514a5
Lunatix14 <102019966+Lunatix14@users.noreply.github.com>
2024-05-07 11:29:40 +0700

    เพิ่ม Script ตรวจสอบคำตอบ

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ function checkAnswer1(answer1) {
     const result = document.getElementById('result1');
     if (answer1 === 'a') {
     result.textContent = "ถูกต้อง! PIM ย่อมาจาก PIM";
-    result.style.color = 'darkgreen';
+    result.style.color = 'lightgreen';
+    document.querySelector('#h41').innerText = "คำถามที่ 1 ถูกต้อง ได้ 1 คะแนน!";
     } else {
     result.textContent = "ผิด! ลองตอบอีกที";
-    result.style.color = 'darkred';
+    result.style.color = 'white';
+    document.querySelector('#h41').innerText = "คำถามที่ 1 ยังไม่มีคะแนน";
     }
     }
 
@@ -13,9 +15,11 @@ function checkAnswer2(answer2) {
     const result = document.getElementById('result2');
     if (answer2 === 'd') {
     result.textContent = "ถูกต้อง! Gosoft คือมูลนิธิที่ไม่แสวงหาผลกำไร!";
-    result.style.color = 'darkgreen';
+    result.style.color = 'lightgreen';
+    document.querySelector('#h42').innerText = "คำถามที่ 2 ถูกต้อง ได้ 1 คะแนน!";
     } else {
-    result.textContent = "ผิด! โลกสวยเกินไปแล้ว";
-    result.style.color = 'darkred';
+    result.textContent = "ผิด! โลกสวยเกินไปแล้ว!";
+    result.style.color = 'white';
+    document.querySelector('#h42').innerText = "คำถามที่ 2 ยังไม่มีคะแนน";
     }
     }
